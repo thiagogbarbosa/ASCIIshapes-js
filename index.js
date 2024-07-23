4ca8edf4d59ca54e713def8f5cfa955aee46e300
thiagogbarbosa <tthiagogb@gmail.com>
2024-07-23 13:49:44 +0100

    New script for circle shape

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
-//JavaScript Basics building a Pyramid
-const character = "#";
-const count = 10;
+//Geometric Shapes Generator
+
+const character = "#"; //Choose the character that will fulfill the shape
+const count = 10; //The max range of the shape
 const rows = [];
 let inverted = false;
+let result = "";
+
+//Function to build the Circle
+function padRowCircle(rowNumber, rowCount){
+                                          //padding logic to keep the diameter
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 *rowNumber) + " ".repeat(rowCount -rowNumber); 
+};
+
+//Loop for the semi-circle
+for ( let i = 1; i<=count ; i++){
+  if (i<=(count/2)){
+    rows.push(padRowCircle(i,count));
+    //build the semmicircle
+  } else {
+    //to invert the semicircle
+    rows.unshift(padRowCircle(i,count));
+  }
+};
+
+for (const row of rows) {
+  result = result + "\n" + row;
+};
+
+console.log(result); 
+
+
+/*
 
 //Padding the rows with the exact number of chatacters and empty space
 function padRow(rowNumber, rowCount) {
@@ -25,14 +53,16 @@ for (let i = 1; i <= count; i++) {
 
 /*for (let i = count; i > 0; i--) {
   rows.push(padRow(i, count));
-}*/
+}
 
 //To eliminate brackets and single quotes of the final result!
 
-let result = "";
+
 
 for (const row of rows) {
   result = result + "\n" + row;
 };
 
 console.log(result); 
+
+*/ 
