673fed925b2ca8b6c781ab25c663d76c150ccbab
thiagogbarbosa <tthiagogb@gmail.com>
2024-07-25 10:33:32 +0100

    Abstracting the code to a function

--- index.js
+++ index.js
@@ -4,46 +4,57 @@
 
 */
 
-const character = "#"; //Choose the character that will fulfill the shape
-const count = 10; //The max range of the shape
-const rows = [];
-let inverted = false;
-let result = "";
+function genPyramid(range,character,inverted){
+
+  const rows = [];
+  let result = "";
+ 
+  //Add Type Validation
+
+  function padRow(rowNumber, rowCount) {
+    return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+  };
+  
+  for (let i = 1; i <= range; i++) {
+    if (inverted) {
+      rows.unshift(padRow(i, range));
+    } else {
+      rows.push(padRow(i, range));
+    }
+  };
+  
+  for (const row of rows) {
+    result = result + "\n" + row;
+  };
+  
+  return console.log(result);
+};
+
+genPyramid(10,"#",true);
 
 
-/*
+
+
+
+
+
+
+
+
+
 
 //Padding the rows with the exact number of chatacters and empty space
-function padRow(rowNumber, rowCount) {
-  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
-};
 
-for (let i = 1; i <= count; i++) {
-  if (inverted) {
-    rows.unshift(padRow(i, count));
-  } else {
-    rows.push(padRow(i, count));
-  }
-};
 
 //Different ways to build the loop for the Pyramid
-
 /*while (rows.length < count) {
   rows.push(padRow(rows.length + 1, count));
 }*/
 
 /*for (let i = count; i > 0; i--) {
   rows.push(padRow(i, count));
-}
+} */
 
 //To eliminate brackets and single quotes of the final result!
 
 
-
-for (const row of rows) {
-  result = result + "\n" + row;
-};
-
-console.log(result); 
-
-*/ 
